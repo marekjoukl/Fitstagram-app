5cf18c09d5cdec683da9220d75423e5fee12eb4f
Marek Joukl <jouklmarek@seznam.cz>
2024-11-24 10:41:42 +0000

    feat: serve frontend files in production and add build/start scripts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,12 @@ import photoRoutes from "./routes/photoRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import groupRoutes from "./routes/groupRoutes.js";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
@@ -16,6 +19,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/photos", photoRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/groups", groupRoutes);
+
+if (process.env.NODE_ENV !== "development") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`);
