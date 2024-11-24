ea3b7edd0154d3a0aa290126b19f23788450a94c
Marek Joukl <jouklmarek@seznam.cz>
2024-11-24 14:03:45 +0000

    fix: correct static file path for production environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,7 @@ import groupRoutes from "./routes/groupRoutes.js";
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config();
-
 const app = express();
-
 const __dirname = path.resolve();
 
 app.use(cookieParser());
@@ -21,12 +19,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/groups", groupRoutes);
 
 if (process.env.NODE_ENV !== "development") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
-
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`);
 });
